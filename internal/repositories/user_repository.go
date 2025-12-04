1ae4235669a41101376b97baa6979a778d412662
Sajad Dehqan <109065711+sedwna@users.noreply.github.com>
2025-12-04 19:28:40 +0330

    add GetByID function

--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -1,34 +1,44 @@
 package repositories
 
 import (
-    "database/sql"
-    "github.com/sedwna/Ticket-reservation/internal/models"
+	"database/sql"
+	"github.com/sedwna/Ticket-reservation/internal/models"
 )
 
 type UserRepository struct {
-    DB *sql.DB
+	DB *sql.DB
 }
 
 func NewUserRepository(db *sql.DB) *UserRepository {
-    return &UserRepository{DB: db}
+	return &UserRepository{DB: db}
 }
 
 // گرفتن همه کاربران
 func (r *UserRepository) GetAllUsers() ([]models.User, error) {
-    rows, err := r.DB.Query("SELECT id, name, phone, password_hash, role, created_at FROM users")
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
+	rows, err := r.DB.Query("SELECT id, name, phone, password_hash, role, created_at FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-    users := []models.User{}
-    for rows.Next() {
-        var u models.User
-        err := rows.Scan(&u.ID, &u.Name, &u.Phone, &u.PasswordHash, &u.Role, &u.CreatedAt)
-        if err != nil {
-            return nil, err
-        }
-        users = append(users, u)
-    }
-    return users, nil
+	users := []models.User{}
+	for rows.Next() {
+		var u models.User
+		err := rows.Scan(&u.ID, &u.Name, &u.Phone, &u.PasswordHash, &u.Role, &u.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+	}
+	return users, nil
+}
+
+func (r *UserRepository) GetByID(id int64) (*models.User, error) {
+	var u models.User
+	err := r.DB.QueryRow("SELECT id, name, phone, role, created_at FROM users WHERE id=$1", id).
+		Scan(&u.ID, &u.Name, &u.Phone, &u.Role, &u.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return &u, nil
 }
