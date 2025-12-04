005779178285dbc7c0c5902ab0943dfb4fce2f7e
Sajad Dehqan <109065711+sedwna@users.noreply.github.com>
2025-12-04 18:50:51 +0330

    add NewUserService, GetAllUsers function

---
+++ internal/services/user_service.go
@@ -0,0 +1,18 @@
+package services
+
+import (
+    "github.com/sedwna/Ticket-reservation/internal/models"
+    "github.com/sedwna/Ticket-reservation/internal/repositories"
+)
+
+type UserService struct {
+    Repo *repositories.UserRepository
+}
+
+func NewUserService(repo *repositories.UserRepository) *UserService {
+    return &UserService{Repo: repo}
+}
+
+func (s *UserService) GetAllUsers() ([]models.User, error) {
+    return s.Repo.GetAllUsers()
+}
